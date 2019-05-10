303fda4f0146a8032cc2cee5cb577afe04c11f25
Aran Clary Deltac <aran@bluefeet.dev>
2019-05-10 13:32:06 -0700

    Checking in changes prior to tagging of version 0.18.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index a3418c7..1387869 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,8 @@ Revision history for Perl extension GIS-Distance.
    
     {{$NEXT}}
    
    +0.18 2019-05-10T20:31:31Z
    +
      - Switch to the GNU General Public License version 3.
      - Fixed pod error as reported by CPANTS.
      - Documentation edits.

--- lib/GIS/Distance.pm
+++ lib/GIS/Distance.pm
@@ -1,7 +1,7 @@
 package GIS::Distance;
 use 5.008001;
 use strictures 2;
-our $VERSION = '0.17';
+our $VERSION = '0.18';
 
 sub new {
     my ($class, $formula, @args) = @_;
--- lib/GIS/Distance/Null.pm
+++ lib/GIS/Distance/Null.pm
@@ -1,7 +1,7 @@
 package GIS::Distance::Null;
 use 5.008001;
 use strictures 2;
-our $VERSION = '0.17';
+our $VERSION = '0.18';
 
 use parent 'GIS::Distance::Formula';
 
